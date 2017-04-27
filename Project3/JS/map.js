0d6a583d8a94753ef748f0e62383013a16dc78f9
Paolo Ratti Tamayo <prattita@hawk.iit.edu>
2017-04-26 21:25:24 -0500

    Add files via upload

---
+++ Project3/JS/map.js
@@ -0,0 +1,16 @@
+var map;
+function initMap() {
+	var myPos = {lat: -16.381416, lng: -71.545611}
+    map = new google.maps.Map(document.getElementById('map'), {
+		center: myPos,
+		zoom: 10
+		
+    });
+	var customIcon = './Pictures/Peruicon.png';
+	var marker = new google.maps.Marker({
+		position: myPos,
+		map: map,
+		icon: customIcon,
+		animation: google.maps.Animation.DROP
+	});
+}
